05005dd0ecc4a39a9aa24d1f02e274d72bea7f0f
Araik7979 <aro797919@gamil.com>
2017-05-04 17:08:11 +0400

    Add  tetxt area comment

--- app.rb
+++ app.rb
@@ -43,9 +43,11 @@ redirect to('/')
  end
 
  get'/details/:post_id' do
+ 	#получаем переменную из url
 post_id = params[:post_id]
-
+#получаем список постов
  @results = @db.execute'select * from Posts where id = ?',[post_id]
+ #выбираем этот один пост в переменную @row
  @row = @results[0]
 erb :details
 end
